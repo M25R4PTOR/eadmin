9798e3435858d4daf046c161dbfea2f97a866a87
formacion <formacion@mvformacion.localdomain>
2018-03-21 12:24:22 +0100

    Añadida Clase BaseAdministracion

--- src/main/java/es/fpdual/eadmin/eadmin/modelo/Documento.java
+++ src/main/java/es/fpdual/eadmin/eadmin/modelo/Documento.java
@@ -2,37 +2,13 @@ package es.fpdual.eadmin.eadmin.modelo;
 
 import java.util.Date;
 
-public class Documento {
+public class Documento extends BaseAdministracion{
 
-	private Integer codigo;
-	private String nombre;
-	private Date fechaCreacion;
-	private Boolean publico;
 	private EstadoDocumento estado;
 
 	public Documento(Integer codigo, String nombre, Date fechaCreacion, Boolean publico, EstadoDocumento estado) {
-		super();
-		this.codigo = codigo;
-		this.nombre = nombre;
-		this.fechaCreacion = fechaCreacion;
-		this.publico = publico;
+		super(codigo, nombre, fechaCreacion, publico);
 		this.estado = estado;
-	}
-
-	public Integer getCodigo() {
-		return codigo;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public Date getFechaCreacion() {
-		return fechaCreacion;
-	}
-
-	public Boolean getPublico() {
-		return publico;
 	}
 
 	public EstadoDocumento getEstado() {
@@ -41,19 +17,19 @@ public class Documento {
 
 	@Override
 	public int hashCode() {
-		return codigo.hashCode();
+		return getCodigo().hashCode();
 	}
 
 	@Override
 	public boolean equals(Object obj) {
 		if (obj instanceof Documento) {
-			return codigo.equals(((Documento) obj).getCodigo());
+			return getCodigo().equals(((Documento) obj).getCodigo());
 		}
 		return false;
 	}
 
 	@Override
 	public String toString() {
-		return "Documento con código " + codigo;
+		return "Documento con código " + getCodigo();
 	}
 }
--- src/main/java/es/fpdual/eadmin/eadmin/modelo/Expediente.java
+++ src/main/java/es/fpdual/eadmin/eadmin/modelo/Expediente.java
@@ -3,42 +3,22 @@ package es.fpdual.eadmin.eadmin.modelo;
 import java.util.Date;
 import java.util.List;
 
-public class Expediente {
+public class Expediente extends BaseAdministracion{
 
-	private Integer codigo;
-	private String nombre;
-	private Date fechaCreacion;
 	private Date fechaArchivado;
-	private Boolean publico;
 	private EstadoExpediente estado;
 	private List<Documento> documentos;
-		
+	
 	public Expediente(Integer codigo, String nombre, Date fechaCreacion, Date fechaArchivado, Boolean publico,
 			EstadoExpediente estado, List<Documento> documentos) {
-		super();
-		this.codigo = codigo;
-		this.nombre = nombre;
-		this.fechaCreacion = fechaCreacion;
+		super(codigo, nombre, fechaCreacion, publico);
 		this.fechaArchivado = fechaArchivado;
-		this.publico = publico;
 		this.estado = estado;
 		this.documentos = documentos;
 	}
 	
-	public Integer getCodigo() {
-		return codigo;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public Date getFechaCreacion() {
-		return fechaCreacion;
-	}
 	public Date getFechaArchivado() {
 		return fechaArchivado;
-	}
-	public Boolean getPublico() {
-		return publico;
 	}
 	public EstadoExpediente getEstado() {
 		return estado;
@@ -49,21 +29,21 @@ public class Expediente {
 	
 	@Override
 	public int hashCode() {
-		return codigo.hashCode() + nombre.hashCode() + fechaCreacion.hashCode() + fechaArchivado.hashCode() + publico.hashCode() + estado.hashCode();
+		return super.hashCode() + fechaArchivado.hashCode() + estado.hashCode();
 	}
 
 	@Override
 	public boolean equals(Object obj) {
 		if (obj instanceof Expediente) {
-			return codigo.equals(((Expediente) obj).getCodigo()) && nombre.equals(((Expediente) obj).getNombre()) 
-					&& fechaCreacion.equals(((Expediente) obj).getFechaCreacion()) && fechaArchivado.equals(((Expediente) obj).getFechaArchivado()) 
-					&& publico.equals(((Expediente) obj).getPublico()) && estado.equals(((Expediente) obj).getEstado());
+			return getCodigo().equals(((Expediente) obj).getCodigo()) && getNombre().equals(((Expediente) obj).getNombre()) 
+					&& getFechaCreacion().equals(((Expediente) obj).getFechaCreacion()) && fechaArchivado.equals(((Expediente) obj).getFechaArchivado()) 
+					&& getPublico().equals(((Expediente) obj).getPublico()) && estado.equals(((Expediente) obj).getEstado());
 		}
 		return false;
 	}
 	
 	@Override
 	public String toString() {
-		return "Documento con código " + codigo;
+		return "Documento con código " + getCodigo();
 	}
 }
